3ed0398f5b11cee9d2e6bc902ecdabacf2cf1d4d
ghaiyyas-azhar <azharghaiyyas@gmail.com>
2025-11-24 19:54:36 +0700

    memasukan file docker

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,16 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "jewelry_store";
+$host = "mysql";            // nama service mysql di docker-compose
+$user = "root";             // user mysql
+$pass = "password";         // password sesuai docker-compose
+$db   = "jewelry_store";    // nama database kamu
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-function db_disconnect ($con) {
+
+function db_disconnect($con) {
     mysqli_close($con);
 }
 ?>
